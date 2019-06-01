cdcb8e0882b05de3cfff249f813a1951485521c2
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-01 06:12:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,14 +70,15 @@ class Student
       end
     end
 
-  def self.first_x_students_in_grade_10(x)
+  def self.first_X_students_in_grade_10(x)
     sql <<-SQL
       SELECT *
       FROM students
       WHERE grade = 10
       LIMIT ?
     SQL
-    DB[:conn].execute(sql).x.collect do |row|
+    
+    DB[:conn].execute(sql, x).collect do |row|
       self.new_from_db(row)
     end
   end
