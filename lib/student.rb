8662638ee0fc47629077eb6a10b353ec14a88aa0
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-01 06:14:53 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -83,6 +83,17 @@ class Student
     end
   end
 
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = ?
+    SQL
+    DB[:conn].execute(sql, x).collect do |row|
+      self.new_from_db(row)
+    end
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
