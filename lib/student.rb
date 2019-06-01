3126d1cdf6f42ce06aa5cc37ac2cf34c785d3258
JASON GUTIERREZ <jasongutierrez1989@gmail.com>
2019-06-01 05:56:18 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -68,6 +68,16 @@ class Student
       end
     end
 
+  def self.first_x_students_in_grade_10(x)
+    sql <<-SQL
+      SELECT *
+      FROM students
+      WHERE grade = 10 LIMIT ?
+    SQL
+    
+    DB[:conn].execute(sql, x)
+  end
+
   def save
     sql = <<-SQL
       INSERT INTO students (name, grade)
